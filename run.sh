810dbd5972f351cdbcceded3ba59e6bff9fc32ea
payt0nc <payton@cooomma.info>
2023-01-11 22:10:33 +0800

    update: mpd and m3u8

--- run.sh
+++ run.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 PACKAGER_BIN="./packager"
 
+CONTENT_ID="contentId"
 KEY_ID="6d76f25cb17f5e16b8eaef6bbf582d8e"
 KEY="cb541084c99731aef4fff74500c12ead"
 IV="33445566778899001122334455667788"
-
-function playready() {
-    ${PACKAGER_BIN} \
-        in=h264_1080p_5000_60fps.mp4,stream=video,output=h264_1080p.mp4,drm_label=HD \
-        --enable_raw_key_encryption \
-        --keys label=HD:key_id=${KEY_ID}:key=${KEY} \
-        --protection_systems Widevine,PlayReady \
-        --mpd_output h264.mpd
-}
 
 function cenc() {
     ${PACKAGER_BIN} \
@@ -24,6 +16,18 @@ function cenc() {
         --hls_master_playlist_output h264_master.m3u8
 }
 
+function cenc_cbcs() {
+    ${PACKAGER_BIN} \
+        in=h264_1080p_5000_60fps.mp4,stream=video,output=h264_1080p.mp4,drm_label=HD \
+        --enable_raw_key_encryption \
+        --protection_scheme cbcs \
+        --keys label=HD:key_id=${KEY_ID}:key=${KEY}:iv=${IV} \
+        --protection_systems Fairplay,Widevine,PlayReady \
+        --mpd_output h264.mpd \
+        --hls_key_uri skd://${CONTENT_ID} \
+        --hls_master_playlist_output h264_master.m3u8
+}
+
 function fairplay() {
     ${PACKAGER_BIN} \
         in=h264_1080p_5000_60fps.mp4,stream=video,output=h264_1080p.mp4,drm_label=HD \
@@ -32,10 +36,45 @@ function fairplay() {
         --keys label=HD:key_id=${KEY_ID}:key=${KEY}:iv=${IV} \
         --protection_systems FairPlay \
         --hls_master_playlist_output h264_master.m3u8 \
-        --hls_key_uri skd://contentID
+        --hls_key_uri skd://${CONTENT_ID}
+}
+
+function playready() {
+    ${PACKAGER_BIN} \
+        in=h264_1080p_5000_60fps.mp4,stream=video,output=h264_1080p.mp4,drm_label=HD \
+        --enable_raw_key_encryption \
+        --keys label=HD:key_id=${KEY_ID}:key=${KEY} \
+        --protection_systems PlayReady \
+        --mpd_output h264.mpd
+}
+
+function widevine() {
+    ${PACKAGER_BIN} \
+        in=h264_1080p_5000_60fps.mp4,stream=video,output=h264_1080p.mp4,drm_label=HD \
+        --enable_raw_key_encryption \
+        --keys label=HD:key_id=${KEY_ID}:key=${KEY} \
+        --protection_systems Widevine \
+        --mpd_output h264.mpd
+}
+
+function widevine_hls() {
+    ${PACKAGER_BIN} \
+        in=h264_1080p_5000_60fps.mp4,stream=video,output=h264_1080p.mp4,drm_label=HD \
+        --enable_raw_key_encryption \
+        --keys label=HD:key_id=${KEY_ID}:key=${KEY} \
+        --protection_systems Widevine \
+        --hls_master_playlist_output h264_master.m3u8
 }
 
 case $1 in
+
+"cenc")
+    cenc
+    ;;
+
+"cenc_cbcs")
+    cenc_cbcs
+    ;;
 
 "fairplay")
     fairplay
@@ -45,8 +84,12 @@ case $1 in
     cenc
     ;;
 
-"cenc")
-    cenc
+"widevine")
+    widevine
+    ;;
+
+"widevine_hls")
+    widevine_hls
     ;;
 
 *)
